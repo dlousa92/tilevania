5fcbd715cabf2eeb21158997a60d0322544134e8
Daniel Lousa <dlousa92@gmail.com>
2023-06-15 13:15:52 -0400

    add arrow velocity and direction

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+
+    Rigidbody2D arrowRigidBody;
+    PlayerMovement player;
+    [SerializeField] float arrowLaunchVelocity = 10f;
+    float arrowSpeed;
+    float arrowDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrowRigidBody = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<PlayerMovement>();
+        arrowSpeed = arrowLaunchVelocity * player.transform.localScale.x;
+        arrowDirection = player.transform.localScale.x > Mathf.Epsilon ? 1 : -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        arrowRigidBody.velocity = new Vector2(arrowSpeed, 0);
+        FlipSprite();
+    }
+
+    void FlipSprite()
+    {
+
+        transform.localScale = new Vector2(arrowDirection, transform.localScale.y);
+    }
+}
